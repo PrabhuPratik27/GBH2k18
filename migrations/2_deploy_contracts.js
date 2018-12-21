5db052ae9e5d93210790882be8baae872ad3814e
Ameya <ameymohite99@gmail.com>
2018-12-21 15:07:06 +0530

    After deleting files

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,11 +1,9 @@
 var Student = artifacts.require("./Student.sol");
-
-module.exports = function(deployer) {
-  deployer.deploy(Student);
-};
-
 var Recruiter  = artifacts.require("./Recruiter.sol");
 
 module.exports = function(deployer) {
+  deployer.deploy(Student);
   deployer.deploy(Recruiter);
+
 };
+ 
