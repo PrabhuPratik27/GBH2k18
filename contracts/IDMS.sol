810a2dce5497354afec73a429ef70cf52187b9a3
Ameya <ameymohite99@gmail.com>
2018-12-21 19:51:58 +0530

    recruiter to students mapping added

--- contracts/IDMS.sol
+++ contracts/IDMS.sol
@@ -1,9 +1,12 @@
 pragma solidity ^0.4.19;
-contract Student {
 
-	/*getter and setter methods are yet to be added as required later*/
+/// TYPE 1- STUDENT , 2 - Recruiter 3 - institute
+
+
+contract IDMS{
 	struct studentProfile{
 		//string username;
+		uint Type;
 		string name;
 		string password;
 		string dob;
@@ -24,27 +27,27 @@ contract Student {
 	uint public studentsCount;
 
 	constructor() public {
-		studentsCount=0;
-		addStudent("vinayak","abcdefgh","1-1-2000","abc@xyz.com","1234567890",0x0,100,false);
+		
+		addStudent(0,"vinayak","abcdefgh","1-1-2000","abc@xyz.com","1234567890",0x0,100,false);
+		addRecruiter(1,"my","code","Hekko",9192929299,"xyz@abc.com");
+		addInstitute(2,"vjti","abcdefgh","1234567890","mumbai");
 	}
 
-	function addStudent(string _name, string _password, string _dob, string _email, string _phone,
+	function addStudent(uint Type,string _name, string _password, string _dob, string _email, string _phone,
 			 address _institute, uint _marks, bool _isVerified) public {
 		studentsCount++;
 		students[studentsCount/*msg.sender*/]=
-			studentProfile(_name,  _password, _dob, _email, _phone, _institute, _marks, _isVerified);
+			studentProfile(Type,_name,  _password, _dob, _email, _phone, _institute, _marks, _isVerified);
 	}
 
-	function setVerified(uint _id){
+	function setVerified(uint _id) public{
 		/*some checking or manually calling this function to set the value below as true */
 		students[_id/*msg.sender*/].isVerified=true;
 	}
 
-}
-
-contract Recruiter {
-
+	//----------------------------------------------------------------------------------------------------------------------------
 	struct Recruit {
+		uint Type;
 		string username;
 		string password;
 		string OrganisationName;
@@ -57,39 +60,80 @@ contract Recruiter {
 	uint public RecruiterCount;
 	mapping(/*RecruiterAddress*/uint=>Recruit) public RecruiterId;
 	
-	function Recruiter () {
-		addRecruiter("my","code","Hekko",9192929299,"xyz@abc.com");
-	}
+	//uint public recruitersStudentsCount;
+	//mapping(uint /*recruiters (adddress)*/ => studentProfile	) recruitersStudents;
 
-	function addRecruiter(string username,string password,string _name,uint256 _contact,string _organisationname) public{
+	function addRecruiter(uint Type,string username,string password,string _name,uint256 _contact,string _organisationname) public{
 		RecruiterCount++;
-		RecruiterId[/*msg.sender*/RecruiterCount] = Recruit(username,password,_name,_contact,_organisationname);
+		RecruiterId[/*msg.sender*/RecruiterCount] = Recruit(Type,username,password,_name,_contact,_organisationname);
 	}
 
-	function getDetails() public view returns(string,string,string,uint256,string){
-		return(RecruiterId[1].username,RecruiterId[1].password,RecruiterId[1].OrganisationName,RecruiterId[1].Contact,RecruiterId[1].OrganisationMail);
+	function getDetails() public view returns(uint,string,string,string,uint256,string){
+		return(RecruiterId[1].Type,RecruiterId[1].username,RecruiterId[1].password,RecruiterId[1].OrganisationName,RecruiterId[1].Contact,RecruiterId[1].OrganisationMail);
 	}
-}
 
-contract Institute{
+	uint public recruitersStudentsCount;
+	mapping(uint/*recruiter address*/ => uint[] /*student address*/) public recruitersStudents;
+
+	function getRecruitersStudents(uint _rid, uint _itr) public view returns(uint/*student address*/){
+		return recruitersStudents[_rid][_itr];
+	}
+
+	function setRecruiterStudents(uint _rid, uint _sid) public {
+		recruitersStudents[_rid].push(_sid);
+	}
+
+	function getRecruitersStudentsCount (uint _rid) public view returns(uint) {
+		return recruitersStudents[_rid].length;
+	}
+	
+
+	// function getRecruitersStudentsCount(uint _rId)public view returns(uint){
+	// 	return recruitersStudents[_rId].length;
+	// }
+
+	// function getRecruitersStudents(uint _rId, uint _i) public view 
+	// returns(uint , string , string , string , string , string ,address , uint ,bool ) {
+	// 	studentProfile s=recruitersStudents[_rId ][_i];
+		
+	// 	//return (s[0],s[1],s[2],s[3],s[4],s[5],s[6],s[7],s[8]);
+	// 			.Type,
+	// 			recruitersStudents[_rId ][_i].name,
+	// 			recruitersStudents[_rId ][_i].password,
+	// 			recruitersStudents[_rId ][_i].dob,
+	// 			recruitersStudents[_rId ][_i].email,
+	// 			recruitersStudents[_rId ][_i].phone,
+	// 			recruitersStudents[_rId ][_i].institute,
+	// 			recruitersStudents[_rId ][_i].marks,
+	// 			recruitersStudents[_rId ][_i].isVerified
+				
+	// 		);
+	// }
+
+	// function setRecruiterStudents(uint _rId, uint _sId){
+	// 	recruitersStudents[_rId].push(students[_sId/*msg.sender*/]);
+
+	// }
+
+	//----------------------------------------------------------------------------------------------------------------------------
+		
+
 
 	struct InstituteStr{
+		uint Type;
 		string instituteName;
 		string password;
-		string phone;
+		string phone; 
 		string addressInstitute;
 	}
 
 	uint public instituteCount;
 	mapping(/*institute Address (address)*/uint=>InstituteStr) public institutes;
 
-	constructor() public{
-		addInstitute("vjti","abcdefgh","1234567890","mumbai");
-	}
-
-	function addInstitute(string _instituteName, string _password, string phone, string _addressInstitute) public {
+	
+	function addInstitute(uint Type,string _instituteName, string _password, string phone, string _addressInstitute) public {
 		instituteCount++;
 		institutes[/*msg.sender*/instituteCount] 
-			= InstituteStr(_instituteName, _password, phone, _addressInstitute );	
+			= InstituteStr(Type,_instituteName, _password, phone, _addressInstitute );	
 	}
 }
--- test/IDMStest.js
+++ test/IDMStest.js
@@ -1,11 +1,10 @@
-var Student = artifacts.require("./Student.sol");
-var Recruiter = artifacts.require("./Recruiter.sol");
+var IDMS = artifacts.require("./IDMS.sol");
 
-contract("Student", function(accounts) {
+contract("IDMS", function(accounts) {
   var electionInstance;
 
   it("initializes with 1 student", function() {
-    return Student.deployed().then(function(instance) {
+    return IDMS.deployed().then(function(instance) {
       return instance.studentsCount();
     }).then(function(count) {
       assert.equal(count, 1);
@@ -13,18 +12,19 @@ contract("Student", function(accounts) {
   });
 
   it("it initializes the students with the correct values", function() {
-    return Student.deployed().then(function(instance) {
+    return IDMS.deployed().then(function(instance) {
       electionInstance = instance;
       return electionInstance.students(1);
     }).then(function(student) {
-      assert.equal(student[0], "vinayak", "contains the correct name");
-      assert.equal(student[1], "abcdefgh", "contains the correct pass");
-      assert.equal(student[2], "1-1-2000", "contains the dob");
-      assert.equal(student[3], "abc@xyz.com", "contains the email");
-      assert.equal(student[4], "1234567890", "contains the phone");
-      assert.equal(student[5], 0x0, "contains the correct institute addr");
-      assert.equal(student[6], 100, "contains the marks");
-      assert.equal(student[7], false, "contains the verification value");
+      assert.equal(student[0], 0, "type of the student ");
+      assert.equal(student[1], "vinayak", "contains the correct name");
+      assert.equal(student[2], "abcdefgh", "contains the correct pass");
+      assert.equal(student[3], "1-1-2000", "contains the dob");
+      assert.equal(student[4], "abc@xyz.com", "contains the email");
+      assert.equal(student[5], "1234567890", "contains the phone");
+      assert.equal(student[6], 0x0, "contains the correct institute addr");
+      assert.equal(student[7], 100, "contains the marks");
+      assert.equal(student[8], false, "contains the verification value");
 
       return electionInstance.students(1);
     });
@@ -36,7 +36,7 @@ contract("Recruiter", function(accounts) {
   var electionInstance;
 
   it("initializes with 1 Recruiter", function() {
-    return Recruiter.deployed().then(function(instance) {
+    return IDMS.deployed().then(function(instance) {
       return instance.RecruiterCount();
     }).then(function(count) {
       assert.equal(count, 1);   /// count === 1 count,1 correct number initialized
@@ -44,15 +44,16 @@ contract("Recruiter", function(accounts) {
   });
 
   it("it initializes the Recruiter with the correct values", function() {
-    return Recruiter.deployed().then(function(instance) {
+    return IDMS.deployed().then(function(instance) {
       electionInstance = instance;
       return electionInstance.RecruiterId(1);
     }).then(function(student) {
-      assert.equal(student[0], "my", "contains the username name");
-      assert.equal(student[1], "code", "contains the correct pass");
-      assert.equal(student[2], "Hekko", "contains the correct organisation name");
-      assert.equal(student[3], 9192929299, "contains contact no");
-      assert.equal(student[4], "xyz@abc.com", "contains the mail");
+      assert.equal(student[0], 1, "is the type of Recruiter");
+      assert.equal(student[1], "my", "contains the username name");
+      assert.equal(student[2], "code", "contains the correct pass");
+      assert.equal(student[3], "Hekko", "contains the correct organisation name");
+      assert.equal(student[4], 9192929299, "contains contact no");
+      assert.equal(student[5], "xyz@abc.com", "contains the mail");
       
       
 
